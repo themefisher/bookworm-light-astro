4df1912601f9e0902d1d8a87bf3cf911bd773c7e
Mark Ross <krazkidd@gmail.com>
2023-12-05 12:24:51 -0700

    Upgrade Astro to v4 (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -18,9 +17,6 @@ export default defineConfig({
       config: {
         applyBaseStyles: false,
       },
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
     }),
     mdx(),
   ],
