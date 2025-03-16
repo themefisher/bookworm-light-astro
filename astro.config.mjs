b39cf73067a68395889d5677effbe121ad14d44c
Al Murad Uzzaman <murad.themefisher@gmail.com>
2025-03-16 15:13:10 +0600

    Updated to Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,31 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import AutoImport from "astro-auto-import";
 import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
 
+let highlighter;
+async function getHighlighter() {
+  if (!highlighter) {
+    const { getHighlighter } = await import("shiki");
+    highlighter = await getHighlighter({ theme: "one-dark-pro" });
+  }
+  return highlighter;
+}
+
 // https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
+  vite: { plugins: [tailwindcss()] },
   integrations: [
     react(),
     sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
     AutoImport({
       imports: [
         "@/shortcodes/Button",
@@ -49,5 +54,6 @@ export default defineConfig({
       wrap: true,
     },
     extendDefaultPlugins: true,
+    highlighter: getHighlighter,
   },
 });
